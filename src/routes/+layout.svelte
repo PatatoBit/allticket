7b26f0fc35ad72b35ed267d3ae15481e66e57e0b
Patato <pathonbusiness@gmail.com>
2023-01-19 21:56:15 +0700

    update twitter head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,22 +3,22 @@
 </script>
 
 <svete:head>
-	<link rel="icon" href="/logo.svg" />
-
-	<meta name="twitter:title" content="หมูเบคอน" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="author" content="Patato" />
+	<meta name="description" content="ความหิวเป็นเหตุสังเกตได้" />
+	<meta name="author" content="หมูเบคอน" />
 	<meta name="author" content="patato" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@Patatotota" />
-	<meta name="twitter:creator" content="@Patatotota" />
-	<meta name="twitter:image" content="https://baconmockup.com/862/470" />
-	<meta property="og:site_name" content="" />
-	<meta name="og:title" content="Ohaiyo!" />
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://baconmockup.com/862/470" />
 	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<meta name="twitter:title" content="หมูเคคอน" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@patatotota" />
+	<meta name="twitter:creator" content="@patatotota" />
+	<meta name="twitter:image" content="https://baconmockup.com/862/470" />
+	<meta property="og:site_name" content="หมูเบคอน" />
+	<meta name="og:title" content="หมูเบคอน" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://baconmockup.com/862/470" />
+	<title>หมูเบคอน - Home</title>
 </svete:head>
 
 <slot />
