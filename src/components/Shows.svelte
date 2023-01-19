3e83e78f2c2b7d409fa9beb46de6b35c008bad3d
Patato <pathonbusiness@gmail.com>
2023-01-19 20:55:25 +0700

    shows image

--- src/components/Shows.svelte
+++ src/components/Shows.svelte
@@ -1,8 +1,23 @@
-<div class="navbar ">
+<div class="flex items-center w-full justify-between p-4">
 	<div class="flex-1">
 		<h1 class="text-blue-900 font-bold text-2xl">Recommended</h1>
 	</div>
 	<div class="flex-none">
 		<button class="btn btn-outline">Show all</button>
 	</div>
+</div>
+
+<!-- Shows -->
+<div class="grid grid-cols-2 sm:grid-cols-6 gap-4 p-4">
+	<div>
+		<img
+			src="https://atkmedia.allticket.com/assets/content/99991/2390011012566TicketCard.jpg"
+			alt="NCT"
+		/>
+	</div>
+	<div><img class="object-cover" src="https://baconmockup.com/410/600" alt="" /></div>
+	<div><img class="object-cover" src="https://baconmockup.com/410/600" alt="" /></div>
+	<div><img class="object-cover" src="https://baconmockup.com/410/600" alt="" /></div>
+	<div><img class="object-cover" src="https://baconmockup.com/410/600" alt="" /></div>
+	<div><img class="object-cover" src="https://baconmockup.com/410/600" alt="" /></div>
 </div>
