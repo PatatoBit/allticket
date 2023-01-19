19c49a57fd33128be9cf124935f0af019119c59d
Patato <pathonbusiness@gmail.com>
2023-01-19 22:00:09 +0700

    navbar spacing and changed title

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,8 +1,10 @@
-<nav class="navbar bg-slate-500">
-	<div class="flex-1">
-		<h1 class="text-white font-bold">All ticket</h1>
-	</div>
-	<div class="flex-none">
-		<button class="btn">Burger</button>
+<nav class="w-full flex justify-center items-center bg-slate-500">
+	<div class="navbar max-w-5xl">
+		<div class="flex-1">
+			<h1 class="text-white text-2xl font-bold">Moo Bacon</h1>
+		</div>
+		<div class="flex-none">
+			<button class="btn">Burger</button>
+		</div>
 	</div>
 </nav>
