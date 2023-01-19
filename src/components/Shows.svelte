0f995f783d1f6904666cb39ae53d0e994f1e73f3
Patato <pathonbusiness@gmail.com>
2023-01-19 20:37:41 +0700

    shows layout

---
+++ src/components/Shows.svelte
@@ -0,0 +1,8 @@
+<div class="navbar ">
+	<div class="flex-1">
+		<h1 class="text-blue-900 font-bold">Recommended</h1>
+	</div>
+	<div class="flex-none">
+		<button class="btn btn-outline">Show all</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import Images from '../components/Images.svelte';
 	import Navbar from '../components/Navbar.svelte';
+	import Shows from '../components/Shows.svelte';
 </script>
 
-<Navbar />
-<Images />
+<main class="flex flex-col justify center w-full min-h-screen">
+	<Navbar />
+	<Images />
+
+	<div class="md:mx-60">
+		<Shows />
+	</div>
+</main>
